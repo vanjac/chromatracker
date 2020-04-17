5bc875b881e20417343508d2d33a0067f8237610
jace v <vantjac@gmail.com>
2020-04-16 18:11:29 -0700

    fast math

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 g++ -g `sdl2-config --cflags` -Iimgui -Iimgui/examples -Iimgui/examples/libs/gl3w \
+    -ffast-math \
     chroma.c instrument.c pattern.c song.c playback.c load_mod.c guimain.cpp test.cpp \
     imgui/imgui*.cpp imgui/examples/imgui_impl_opengl3.cpp imgui/examples/imgui_impl_sdl.cpp imgui/examples/libs/gl3w/GL/gl3w.c \
     -lSDL2 -lGL -ldl
